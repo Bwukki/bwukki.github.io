6e7ab540ac5f8f2d6899c789386f12bf51bffb0b
Unknown <bwukki@gmail.com>
2017-10-20 05:42:33 -0700

    sketch stuff

--- p5 stuff/drawing/sketch.js
+++ p5 stuff/drawing/sketch.js
@@ -7,9 +7,9 @@ function centerCanvas() {
 }
 
 function setup() {
-  cnv = createCanvas(1000, 400);
+  cnv = createCanvas(1000, 800);
   centerCanvas();
-  background(50, 200, 230);
+  background(200, 200, 230);
 }
 
 function windowResized() {
@@ -17,14 +17,29 @@ function windowResized() {
 }
 
 function draw() {
-  if (mouseX !== pmouseX || mouseY !== pmouseY) {
-    if (mouseIsPressed) {
-      fill(0,255,0);
-    }
-    else {
-      fill(random(100),random(100),random(100));
-    }
+  fill(30,40,200, 100);
+y = 0
+f = -100
+  for (let x = 0; x <= 79; x += 1) {
+	if (x%10 == 0) {
+		f = f+100
+		y = 0
+	  }
+    fill(y * 50, x * 4, y * 20, x);
+    triangle(100*y, f+100, 100+100*y, f+100, 50+100*y, f)
+	y+=1
   }
-  rect(mouseX, mouseY, 80, 80);
-  //ellipse(mouseX, mouseY, 80, 80);
 }
+
+
+
+ // if (mouseX !== pmouseX || mouseY !== pmouseY) {
+   // if (mouseIsPressed) {
+     // fill(0,255,0);
+  //  }
+   // else {
+   //   fill(random(100),random(100),random(100));
+  //  }
+ // }
+ // rect(mouseX, mouseY, 80, 80);
+  //ellipse(mouseX, mouseY, 80, 80);
