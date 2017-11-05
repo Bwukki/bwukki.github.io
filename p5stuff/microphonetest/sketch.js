ef3a7697fa61eb86a0f97bb41d7f87e76c3e552c
Unknown <bwukki@gmail.com>
2017-11-05 14:29:31 -0800

    Added microphone sensitivity button

--- p5stuff/microphonetest/sketch.js
+++ p5stuff/microphonetest/sketch.js
@@ -1,20 +1,38 @@
 var mic;
 var xPos = 0;
 var yPos = 0;
-
+var sensitivity = 5;
 
 function setup() {
   createCanvas(800, 600);
   mic = new p5.AudioIn();
   mic.start();
   frameRate(60);
+//
+
+
+//
+}
+
+function sensChange() {
+ sensitivity = prompt("Please enter a microphone sensitivty between 1 and 20", "");
+  while (isNaN(sensitivity)) {
+   sensitivity = prompt("Please only enter a number between 1 and 20", "");
+  }
+  while (sensitivity > 20) {
+   sensitivity = prompt("Please only enter a number from 1 to 20", "");
+  }
+  while (sensitivity < 1) {
+   sensitivity = prompt("Please only enter a number from 1 to 20", "");
+  }
+
 }
 
 
 function draw() {
   if (yPos <= 0) { yPos = height-50}
   background(200);
-  var vol = mic.getLevel()*100
+  var vol = mic.getLevel()*100*sensitivity
   xPos = xPos + vol
 //if you hold down mouse1 or 2 the ball will rise
   if ( mouseIsPressed ){
@@ -26,16 +44,14 @@ function draw() {
     xPos = 0
   }
   if (yPos > height-50) {
-    yPos = 0
-  }
-  if (yPos < 0 ) {
-    yPos = 0
+    yPos = height-50
   }
   //changes the color if the mic is getting input
   fill(200*vol,100-vol*Math.random()*100,100+vol);
   stroke(0);
   //draws an eclipse, moves to the right at a speed based on the volume from the microphone
   ellipse(50+xPos, yPos, 100, 100);
+  yPos=yPos+5;
 }
 
 
