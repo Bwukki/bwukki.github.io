ef0cac2cd5c57d2e77fcfa1ea88e5b8eb6ade09d
Unknown <bwukki@gmail.com>
2017-11-07 14:48:35 -0800

    Added a ledge with hit detection up top!

--- p5stuff/microphonetest/sketch.js
+++ p5stuff/microphonetest/sketch.js
@@ -2,6 +2,8 @@ var mic;
 var xPos = 0;
 var yPos = 0;
 var sensitivity = 3;
+//VARIABLES FOR COLLISION OBJECT 1
+
 
 function setup() {
   createCanvas(800, 600);
@@ -24,30 +26,53 @@ function sensChange() {
 
 }
 
+function fall() {
+    //checks to see if the position of the ball is above and over the line
+    if (yPos < height/2 && yPos >= height/2-50 && xPos >= width-299 ) {
+
+    }
+    //makes the ball fall
+    else {
+      yPos=yPos+5;
+    }
+}
+
+function drawObstacle() {
+  fill(200,0,0)
+  var ob1x1 = width-200;
+  var ob1x2 = width;
+  var ob1y1 = height/2;
+  var ob1y2 = height/2+20;
+  rect(ob1x1, ob1y1, 200, 20);
+}
 
 function draw() {
+  //ceiling
   if (yPos <= 0+50) { yPos = 0+50}
-  background(200);
-  var vol = mic.getLevel()*100*sensitivity
-  xPos = xPos + vol
-//if you hold down mouse1 or 2 the ball will rise
-  if ( mouseIsPressed ){
-    yPos = yPos - vol
-  }
-
-//the following few if statements just check if the ball is still on the canvas and resets it if its not
-  if (xPos > width) {
-    xPos = 0
-  }
+  // floor
   if (yPos > height-50) {
     yPos = height-50
   }
+  background(200);
+  var vol = mic.getLevel()*100*sensitivity
+  //Updates the x position of the ball based on volume
+  xPos = xPos + vol
+  //Makes ball rise based on volume
+  if ( mouseIsPressed ){
+    yPos = yPos - vol
+  }
+//wrap around (right side only)
+  if (xPos > width) {
+    xPos = 0
+  }
+
   //changes the color if the mic is getting input
   fill(200*vol,100-vol*Math.random()*100,100+vol);
   stroke(0);
   //draws an eclipse, moves to the right at a speed based on the volume from the microphone
   ellipse(50+xPos, yPos, 100, 100);
-  yPos=yPos+5;
+  drawObstacle();
+ fall();
 }
 
 
