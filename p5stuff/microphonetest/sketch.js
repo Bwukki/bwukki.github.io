123b4222b1d887adc93de516b4c468569d416a3f
Unknown <bwukki@gmail.com>
2017-11-24 15:14:57 -0800

    Redesigned game

--- p5stuff/microphonetest/sketch.js
+++ p5stuff/microphonetest/sketch.js
@@ -10,6 +10,14 @@ function setup() {
   mic = new p5.AudioIn();
   mic.start();
   frameRate(60);
+  ob1x1 = width
+  ob1y1 = 4/10 * height;
+  ob2x1 = width
+  ob2y1 = 5/10 * height;
+  ob3x1 = width
+  ob3y1 = height-50
+  difficultyspeed = 10
+
 }
 
 function sensChange() {
@@ -28,22 +36,50 @@ function sensChange() {
 
 function fall() {
     //checks to see if the position of the ball is above and over the line
-    if (yPos < height/2 && yPos >= height/2-50 && xPos >= width-299 ) {
-
+    shouldifall = 1
+    if (ob1x1 >= width/4-250 && ob1x1 <= width/4+100 && yPos >= ob1y1-50 && yPos <= ob1y1+20) {
+      
+      shouldifall = 0
+    }
+    if (ob2x1 >= width/4-250 && ob2x1 <= width/4+100 && yPos >= ob2y1-50 && yPos <= ob2y1+20) {
+     
+      shouldifall = 0
+    }
+    if (ob3x1 >= width/4-250 && ob3x1 <= width/4+100 && yPos >= ob3y1-50 && yPos <= ob3y1+20) {
+     
+      shouldifall = 0
     }
     //makes the ball fall
-    else {
-      yPos=yPos+5;
+    if (shouldifall == 1) {
+      yPos=yPos+7;
     }
 }
 
 function drawObstacle() {
   fill(200,0,0)
-  var ob1x1 = width-200;
   var ob1x2 = width;
-  var ob1y1 = height/2;
   var ob1y2 = height/2+20;
+  var ob2x2 = width;
+  var ob2y2 = height/2+20;
+  var ob3x2 = width;
+  var ob3y2 = height/2+20;
+  ob1x1 = ob1x1-difficultyspeed
+  ob2x1 = ob2x1-difficultyspeed/2
+  ob3x1 = ob3x1-difficultyspeed/4
+  if (ob1x1 < 0-200) {
+    ob1x1 = width
+    ob1y1 = Math.floor((Math.random() * 7.25) + 1)/10 * height;
+  }
+  if (ob2x1 < 0-200) {
+    ob2x1 = width
+    ob2y1 = Math.floor((Math.random() * 7.25) + 1)/10 * height;
+  }
+  if (ob3x1 < 0-200) {
+    ob3x1 = width
+  }
   rect(ob1x1, ob1y1, 200, 20);
+  rect(ob2x1, ob2y1, 200, 20);
+  rect(ob3x1, ob3y1, 200, 20);
 }
 
 function draw() {
@@ -54,13 +90,12 @@ function draw() {
     yPos = height-50
   }
   background(200);
-  var vol = mic.getLevel()*30*sensitivity
+  var vol = mic.getLevel()*20*sensitivity
   //Updates the x position of the ball based on volume
   xPos = xPos + vol
   //Makes ball rise based on volume
-  if ( mouseIsPressed ){
     yPos = yPos - vol
-  }
+  
 //wrap around (right side only)
   if (xPos > width) {
     xPos = 0
@@ -70,7 +105,7 @@ function draw() {
   fill(200*vol,100-vol*Math.random()*100,100+vol);
   stroke(0);
   //draws an eclipse, moves to the right at a speed based on the volume from the microphone
-  ellipse(50+xPos, yPos, 100, 100);
+  ellipse(width/4, yPos, 100, 100);
   drawObstacle();
  fall();
 }
